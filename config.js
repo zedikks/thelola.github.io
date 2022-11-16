d7f18b621fd910dd2c990da57a2125b8b833e7a5
zedikks <117998598+zedikks@users.noreply.github.com>
2022-11-16 22:05:45 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+/*!
+ * Quara Theme by OllieJW (https://olliejw.me)
+ * License - https://olliejw.me/tos
+ */
+
+var server_name = "LolaShield";
+var server_ip = "mc.lolashield.ru";
+var server_port = 25565;
+var ip_copied = "Скопировано!";
+
+var firefly_count = 20;
+
+staff("CleffiBan", "78ed891f-25d4-4a6d-98a0-8fce6cc1e139", ""); 
+staff("zediksafk", "37a17a7b-b275-449e-8619-05a71e767f19", ""); 
+staff("flapyheh", "387702c9-dc2f-4ba9-97b1-6cc181df1e0c", "");
+
+vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
+vote("NameMC", "https://cdn2.downdetector.com/static/uploads/c/300/d7015/namemc.png", "https://ru.namemc.com/server/mc.lolashield.ru?q=mc.lolashield.ru");
+vote("Minecraft MP", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraft-mp.com/");
